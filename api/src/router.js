c217fa80ba5b093e21b52ddf5308efb5769d5979
Sean Hoffman <sean-hoffman@pluralsight.com>
2019-12-16 13:42:08 -0700

    incomplete mod 4

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const router = Router();
 
@@ -23,9 +24,14 @@ const fileFilter = (req, file, callback) => {
 
 const upload = multer({ fileFilter, storage });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if (request.fileValidationError) {
         return response.status(400).json({ error: request.fileValidationError });
+    }
+    try {
+        await imageProcessor(request.file.filename);
+    } catch (error) {
+
     }
 
     return response.status(201).json({ success: true });
