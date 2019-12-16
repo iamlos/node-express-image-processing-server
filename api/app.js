1b33a57278e0f5998afa41c57cd2394e46845557
Sean Hoffman <sean-hoffman@pluralsight.com>
2019-12-16 00:37:05 -0700

    Module 2

--- api/app.js
+++ api/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const router = require('./src/router.js');
 
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', router);
 
 app.use('/*', function (request, response) { response.sendFile(pathToIndex) });
 
